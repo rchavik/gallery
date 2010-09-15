b115edecd92f735d2eff33c6fe0068c8d388289b
Edinei L. Cipriani <phpedinei@gmail.com>
2010-09-14 22:25:21 -0700

    Add a new Route for /gallery

--- config/gallery_routes.php
+++ config/gallery_routes.php
@@ -1,4 +1,5 @@
 <?php
+CroogoRouter::connect('/gallery', array('plugin' => 'gallery', 'controller' => 'albums', 'action' => 'index'));
     CroogoRouter::connect('/gallery/albums', array('plugin' => 'gallery', 'controller' => 'albums', 'action' => 'index'));
 	CroogoRouter::connect('/gallery/album/:slug', array('plugin' => 'gallery', 'controller' => 'albums', 'action' => 'view'), array('pass' => array('slug')));
 ?>
