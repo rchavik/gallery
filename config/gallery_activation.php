8ce36ded52c15b6e9d0fa0df99e70b3b0006c88b
Rachman Chavik <rchavik@xintesa.com>
2011-07-07 12:51:11 +0700

    space hunt

--- config/gallery_activation.php
+++ config/gallery_activation.php
@@ -22,12 +22,12 @@ class GalleryActivation {
  * @param object $controller Controller
  * @return void
  */
-    public function onActivation(&$controller) {
-        // ACL: set ACOs with permissions
+	public function onActivation(&$controller) {
+		// ACL: set ACOs with permissions
 		$controller->Croogo->addAco('Gallery/Albums');
-        $controller->Croogo->addAco('Gallery/Albums/index', array('registered', 'public'));
-        $controller->Croogo->addAco('Gallery/Albums/view', array('registered', 'public'));
-        $controller->Croogo->addAco('Gallery/Albums/admin_index', array('admin'));
+		$controller->Croogo->addAco('Gallery/Albums/index', array('registered', 'public'));
+		$controller->Croogo->addAco('Gallery/Albums/view', array('registered', 'public'));
+		$controller->Croogo->addAco('Gallery/Albums/admin_index', array('admin'));
 		$controller->Croogo->addAco('Gallery/Albums/admin_add', array('admin'));
 		$controller->Croogo->addAco('Gallery/Albums/admin_edit', array('admin'));
 		$controller->Croogo->addAco('Gallery/Photos');
@@ -38,11 +38,11 @@ class GalleryActivation {
 
 		$controller->Setting->write('Gallery.album_limit_pagination', '10', array('editable' => 1, 'title' => 'Albums Per Page'));
 		$controller->Setting->write('Gallery.max_width', '500', array('editable' => 1));
-    	$controller->Setting->write('Gallery.max_width_thumb', '120', array('editable' => 1));
-    	$controller->Setting->write('Gallery.max_height_thumb', '80', array('editable' => 1));
-    	$controller->Setting->write('Gallery.quality', '90', array('editable' => '1'));
+		$controller->Setting->write('Gallery.max_width_thumb', '120', array('editable' => 1));
+		$controller->Setting->write('Gallery.max_height_thumb', '80', array('editable' => 1));
+		$controller->Setting->write('Gallery.quality', '90', array('editable' => '1'));
 		$controller->Setting->write('Gallery.jslibs', 'galleria,nivo-slider,DDSlider', array('editable' => '1'));
-    }
+	}
 
 	public function beforeDeactivation(&$controller) {
 		return true;
@@ -54,15 +54,15 @@ class GalleryActivation {
  * @param object $controller Controller
  * @return void
  */
-    public function onDeactivation(&$controller) {
-        $controller->Croogo->removeAco('Gallery');
+	public function onDeactivation(&$controller) {
+		$controller->Croogo->removeAco('Gallery');
 
 		$controller->Setting->deleteKey('Gallery.album_limit_pagination');
 		$controller->Setting->deleteKey('Gallery.max_width');
-    	$controller->Setting->deleteKey('Gallery.max_width_thumb');
-    	$controller->Setting->deleteKey('Gallery.max_height_thumb');
-    	$controller->Setting->deleteKey('Gallery.quality');
-    	$controller->Setting->deleteKey('Gallery.jslibs');
-    }
+		$controller->Setting->deleteKey('Gallery.max_width_thumb');
+		$controller->Setting->deleteKey('Gallery.max_height_thumb');
+		$controller->Setting->deleteKey('Gallery.quality');
+		$controller->Setting->deleteKey('Gallery.jslibs');
+	}
 }
 ?>
--- config/gallery_routes.php
+++ config/gallery_routes.php
@@ -1,5 +1,13 @@
 <?php
-CroogoRouter::connect('/gallery', array('plugin' => 'gallery', 'controller' => 'albums', 'action' => 'index'));
-    CroogoRouter::connect('/gallery/albums', array('plugin' => 'gallery', 'controller' => 'albums', 'action' => 'index'));
-	CroogoRouter::connect('/gallery/album/:slug', array('plugin' => 'gallery', 'controller' => 'albums', 'action' => 'view'), array('pass' => array('slug')));
-?>
+CroogoRouter::connect('/gallery',
+	array('plugin' => 'gallery', 'controller' => 'albums', 'action' => 'index')
+	);
+
+CroogoRouter::connect('/gallery/albums',
+	array('plugin' => 'gallery', 'controller' => 'albums', 'action' => 'index')
+	);
+
+CroogoRouter::connect('/gallery/album/:slug',
+	array('plugin' => 'gallery', 'controller' => 'albums', 'action' => 'view'),
+	array('pass' => array('slug')
+	));
