e2541e8838e33643926f35e52f4e5fc6dd2d410f
arie <asutanto@xintesa.com>
2011-10-13 15:36:08 +0700

    set edit photo for admin

--- controllers/photos_controller.php
+++ controllers/photos_controller.php
@@ -19,7 +19,27 @@ class PhotosController extends GalleryAppController {
 		'Containable',
 		);
 
+	function admin_edit($id = null) {
+		if (!$id) {
+			$this->Session->setFlash(__('Invalid Photo.', true));
+			$this->redirect(array('action' => 'index'));
+		}
+		$photo = $this->Photo->findById($id);
+		$albumId = $this->Photo->Album->findById($photo['Photo']['album_id']);
+		$redirect = array('controller' => 'albums', 'action' => 'upload', $albumId['Album']['id']);
+		if (!empty($this->data)) {
+			if ($this->Photo->save($this->data)) {
+				$this->Session->setFlash(__d('gallery', 'Photo is saved.', true));
+				$this->redirect($redirect);
+			} else {
+				$this->Session->setFlash(__d('gallery', 'Photo is saved.', true));
+			}
+		}
+		$albums = $this->Photo->Album->find('list');
 
-
+		$this->set('title_for_layout',__d('gallery', 'Edit Photo', true));
+		$this->data = $this->Photo->read(null, $id);
+		$this->set(compact(array('albums')));
+	}
 }
 ?>
