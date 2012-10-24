d26b7d6c30f205cc9104295717d968edee4e983c
Rachman Chavik <rchavik@xintesa.com>
2012-10-24 09:53:40 +0700

    configurable useDbConfig

--- Model/GalleryAppModel.php
+++ Model/GalleryAppModel.php
@@ -1,10 +1,16 @@
 <?php
 class GalleryAppModel extends AppModel {
 
-	public $useDbConfig = 'gallery';
-
 	public $actsAs = array(
 		'Containable',
 		);
+
+	public function __construct($id = false, $table = null, $ds = null) {
+		$useDbConfig = Configure::read('Gallery.useDbConfig');
+		if ($useDbConfig) {
+			$this->useDbConfig = $useDbConfig;
+		}
+		parent::__construct($id, $table, $ds);
+	}
 
 }
