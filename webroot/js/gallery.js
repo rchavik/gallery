8ffe4b0e5f98a833db62684c18556b754f4b79c5
Rachman Chavik <rchavik@xintesa.com>
2013-12-10 08:14:24 +0700

    Add length check as per https://github.com/rchavik/gallery/pull/2#issuecomment-26611049

--- webroot/js/gallery.js
+++ webroot/js/gallery.js
@@ -1,7 +1,8 @@
 if (typeof Croogo !== 'undefined') {
 	if (typeof jQuery !== 'undefined') {
 		$(function() {
-			if($('#AlbumTitle')){
+			var $albumTitle = $('#AlbumTitle');
+			if ($albumTitle.length > 0) {
 				$('#AlbumTitle').slug({
 					slug: '#AlbumSlug',
 					hide: false
